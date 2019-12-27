8877b5a0483c21ba716d14cfdd72bb61ea693061
Ales Cepek <cepek@gnu.org>
2019-12-27 17:13:04 +0100

    initial commit

--- init.el
+++ init.el
@@ -37,16 +37,3 @@
 ;(require 'ccls)
 ;(setq ccls-executable "/home/cepek/ccls/Release/ccls")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ede-project-directories (quote ("/home/cepek/work")))
- '(package-selected-packages (quote (company irony))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
